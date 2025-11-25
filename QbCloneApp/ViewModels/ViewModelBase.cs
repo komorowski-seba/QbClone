ca4d13cf1cda1a4d3be6ff5ecfb347594ec1172f
seba <komorowski.seba@gmail.com>
2025-11-25 21:25:46 +0100

    add app

---
+++ QbCloneApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace QbCloneApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ QbCloneApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace QbCloneApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
